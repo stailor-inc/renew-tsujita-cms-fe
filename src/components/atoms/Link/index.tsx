9ab03b381aff4d6fbdc41257c20831e6c26ae241
Coraline (Jitera) <developers@jitera.com>
2024-06-19 10:24:35 +0000

    feat: UI code from Figma

--- src/components/atoms/Link/index.tsx
+++ src/components/atoms/Link/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { useTranslation } from 'next-i18next';
+import clsx from 'clsx';
 import styles from './index.module.css';
 
 export interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   translationKey: string;
+  style?: React.CSSProperties; // Added style prop for custom styling
+  className?: string; // Added className prop for custom class names
+  containerStyle?: React.CSSProperties; // Added containerStyle prop for custom styling of the container
 }
 
-const Link: React.FC<LinkProps> = ({ translationKey, ...props }) => {
+const Link: React.FC<LinkProps> = ({ translationKey, style, className, containerStyle, ...props }) => {
   const { t } = useTranslation();
 
   return (
-    <a className={styles.link} {...props}>
-      {t(translationKey)}
-    </a>
+    <div style={containerStyle}>
+      <a className={clsx(styles.link, className)} style={style} {...props}>
+        {t(translationKey)}
+      </a>
+    </div>
   );
 };
 
